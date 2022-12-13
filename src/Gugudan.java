f3d66155b3ce42f7f1fde9a2233b9a4430632123
hseoklee <lhs920619@gmail.com>
2022-12-14 08:48:12 +0900

    gugudan

---
+++ src/Gugudan.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Gugudan {
+	public static void main(String[] args) {
+		int[] result = new int[9];
+		for(int i = 0; i < result.length; i++) {
+			result[i] = 2*(i+1);
+		}
+		
+		for (int i = 0; i < result.length; i++)
+			System.out.println(result[i]);
+	}
+ 
+}
+
+
